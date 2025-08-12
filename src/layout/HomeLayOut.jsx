3d827267b8d22c7f6b5ef220786e644fb001006c
Nahid-07 <nahidasif1998@gmail.com>
2025-08-12 16:50:33 +0600

    layout set

--- src/layout/HomeLayOut.jsx
+++ src/layout/HomeLayOut.jsx
@@ -1,17 +1,20 @@
-import { Header } from "../components/Header"
-import Navbar from "../components/NavBar"
-
+import { Header } from "../components/Header";
+import Navbar from "../components/NavBar";
 
 export const HomeLayOut = () => {
   return (
     <div>
-        <header className="flex w-11/12 mx-auto justify-center items-center">
-            <Header/>
-        </header>
-        <nav>
-          <Navbar/>
-        </nav>
-        <main></main>
+      <header className="flex w-11/12 mx-auto justify-center items-center">
+        <Header />
+      </header>
+      <nav>
+        <Navbar />
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 grid grid-cols-12 gap-3">
+        <aside className="col-span-3">left side</aside>
+        <section className="col-span-6">Middle content</section>
+        <aside className="col-span-3">right side content</aside>
+      </main>
     </div>
-  )
-}
+  );
+};
