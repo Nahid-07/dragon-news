a0e82ea1e87471b9bfba165d7d6ef7fccf2d11eb
Nahid-07 <nahidasif1998@gmail.com>
2025-08-15 16:56:34 +0600

    sign up method built

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../provider/Context";
 
 export const Register = () => {
+  const {createUserWithEmail} = useContext(AuthContext)
+  const handleRegister = (event)=>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const registerInfomation = {
+      name : name,
+      photo: photo,
+      email: email,
+      password: password
+
+    }
+    createUserWithEmail(registerInfomation.email, registerInfomation.password)
+  }
   return (
     <div className="flex items-center justify-center h-screen -mt-24">
       <div className="bg-white p-8 rounded-lg lg:w-4/12 md:w-8/12 w-11/12">
@@ -10,7 +28,7 @@ export const Register = () => {
         </h2>
         <hr className="mb-6" />
 
-        <form className="space-y-5">
+        <form onSubmit={handleRegister} className="space-y-5">
           {/* Email */}
           <div>
             <label className="block text-sm font-semibold mb-2">
@@ -18,31 +36,37 @@ export const Register = () => {
             </label>
             <input
               type="text"
+              name="name"
               className="w-full px-4 py-3 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
             />
           </div>
 
-          {/* Password */}
+          {/* photo */}
           <div>
             <label className="block text-sm font-semibold mb-2">
               Photo URL
             </label>
             <input
               type="text"
+              name="photo"
               className="w-full px-4 py-3 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
             />
           </div>
+          {/* email */}
           <div>
             <label className="block text-sm font-semibold mb-2">Email</label>
             <input
               type="email"
+              name="email"
               className="w-full px-4 py-3 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
             />
           </div>
+        {/* password */}
           <div>
             <label className="block text-sm font-semibold mb-2">Password</label>
             <input
               type="Password"
+              name="password"
               className="w-full px-4 py-3 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
             />
           </div>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,18 +1,44 @@
-import { useState } from "react"
-import { AuthContext } from "./Context"
+import { useEffect, useState } from "react";
+import { AuthContext } from "./Context";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
 
-export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState({
-        name: "Asif"
-    })
-    const authInfo = {
-        user,
-        setUser
-    }
+const auth = getAuth(app);
 
-      return (
-    <AuthContext.Provider value={authInfo}>
-        {children}
-    </AuthContext.Provider>
-  )
-}
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  // create user with email and password
+  const createUserWithEmail = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+  const authInfo = {
+    user,
+    setUser,
+    createUserWithEmail,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
